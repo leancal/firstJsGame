a37f651f07c382e8df21878b3010cf1347cb0f43
Leandro Calvet <leandrocalvet14@gmail.com>
2022-10-21 12:26:15 -0300

    Add enemies

---
+++ enemies/js/script.js
@@ -0,0 +1,41 @@
+/** @type {HTMLCanvasElement} */
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+const numberEnemies = 100;
+const enemiesArray = [];
+
+class Enemy{
+    constructor(){
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.width = 100;
+        this.height = 100;
+        this.speed = Math.random() * 4 - 2;
+    }
+    update(){
+        this.x++;
+        this.y++;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+
+for(let i = 0; i < numberEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // enemy1.update();
+    // enemy1.draw();
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    requestAnimationFrame(animate);
+}
+animate();
