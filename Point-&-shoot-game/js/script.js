87f12eb0a378e17f6dfe7027055df22402e9ca8d
Leandro Calvet <leandrocalvet14@gmail.com>
2022-10-24 09:35:35 -0300

    point & shoot

--- Point-&-shoot-game/js/script.js
+++ Point-&-shoot-game/js/script.js
@@ -10,20 +10,29 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor() {
-        this.width = 100;
-        this.height = 50;
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
+        this.makedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'assets/img/raven.png';
+        
         
     }
     update() {
         this.x -= this.directionX;
+        if(this.x < 0 - this.width) this.makedForDeletion = true;
     }
     
     draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
@@ -41,6 +50,7 @@ function animate(timestamp){
     
     [...ravens].forEach(object => object.update());
     [...ravens].forEach(object => object.draw());
+    ravens = ravens.filter(object => !object.makedForDeletion);
     requestAnimationFrame(animate);
 }
 animate(0);
