2c8e2558f7feac83d4056377e88bff27f17be5e6
Leandro Calvet <leandrocalvet14@gmail.com>
2022-10-26 18:46:16 -0300

    8

--- 8-state-managment/js/player.js
+++ 8-state-managment/js/player.js
@@ -1,16 +1,23 @@
-class Player{
-    constructor(){
+import { StandingLeft, StandingRight } from "./state.js";
+export default class Player{
+    constructor(gameWidth, gameHeight){
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.states = [];
+        this.states = [new StandingLeft(this), new StandingRight(this)];
         this.currentState = this.states[0];
         this.image = document.getElementById('dogImage');
         this.width = 200;
         this.height = 181.83;
-        this.x = 0;
-        this.y = 0;
+        this.x = this.gameWidth / 2 - this.width / 2;
+        this.y = this.gameHeight - this.height;
+        this.frameX = 0;
+        this.frameY = 0;
     }
     draw(context){
-        context.drawImage(this.image, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.width * this.frameX, this.height *  this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
