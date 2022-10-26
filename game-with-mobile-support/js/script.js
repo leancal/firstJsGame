b69c50a469a1dc0e91fee26543c9ad6a10c6c8d5
Leandro Calvet <leandrocalvet14@gmail.com>
2022-10-26 09:59:08 -0300

    End mobile 7

--- game-with-mobile-support/js/script.js
+++ game-with-mobile-support/js/script.js
@@ -96,13 +96,14 @@ window.addEventListener("load", function () {
         this.height
       );
     }
+
     update(input, deltaTime, enemies) {
       //collision detection
       enemies.forEach((enemy) => {
-        const dx = enemy.x + enemy.width / 2 - (this.x + +this.width / 2);
-        const dy = enemy.y + enemy.height / 2 - (this.y + this.height / 2);
+        const dx = (enemy.x + enemy.width / 2 - 20) - (this.x + +this.width / 2);
+        const dy = (enemy.y + enemy.height / 2) - (this.y + this.height / 2 + 20);
         const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < enemy.width / 2 + this.width / 2) {
+        if (distance < enemy.width / 3 + this.width / 3) {
           gameOver = true;
         }
       });
@@ -211,6 +212,7 @@ window.addEventListener("load", function () {
         this.width,
         this.height
       );
+      
     }
     update(deltaTime) {
       if (this.frameTimer > this.frameInterval) {
@@ -304,7 +306,7 @@ window.addEventListener("load", function () {
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
-    //background.update();
+    background.update();
     player.draw(ctx);
     player.update(input, deltaTime, enemies);
     handleEnemies(deltaTime);
