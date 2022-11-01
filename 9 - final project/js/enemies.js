8b5e4d73f3605e3b3537758e1e0668b6283d113c
Leandro Calvet <leandrocalvet14@gmail.com>
2022-11-01 10:35:08 -0300

    9 - final project - fix background

--- 9 - final project/js/enemies.js
+++ 9 - final project/js/enemies.js
@@ -5,10 +5,11 @@ class Enemy {
         this.fps = 20;
         this.frameInterval = 1000 / this.fps;
         this.frameTimer = 0;
+        this.markedForDeletion = false;
     }
     update(deltaTime){
         //movement
-        this.x += this.speedX;
+        this.x -= this.speedX + this.game.speed;
         this.y += this.speedY;
         if(this.frameTimer > this.frameInterval){
             this.frameTimer = 0;
@@ -17,33 +18,29 @@ class Enemy {
         } else {
             this.frameTimer += deltaTime;
         }
+        //check if off screen
+        if(this.x + this.width < 0) this.markedForDeletion = true;
     }
     draw(context){
-        context.drawImage(this.image);
+        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
 }
 
-class FlyingEnemy extends Enemy {
+export class FlyingEnemy extends Enemy {
     constructor(game){
         super();
         this.game = game;
         this.width = 60;
         this.height = 44;
-        this.x = 200;
-        this.y = 200;
-        this.speedX = 2;
+        this.x = this.game.width + Math.random() * this.game.width * 0.5;
+        this.y = Math.random() * (this.game.height * 0.5);
+        this.speedX = Math.random() * (this.game.maxSpeed - 1) + 1;
+        this.speedY = 0;
         this.maxFrame = 5;
         this.image = document.getElementById('enemy_fly');
     }
-    update(){
-        this.x -= this.speed;
-        if (this.frameTimer > this.frameInterval) {
-            this.frameTimer = 0;
-            if (this.frameX < 3) this.frameX++;
-            else this.frameX = 0;
-        } else {
-            this.frameTimer += this.game.deltaTime;
-        }
+    update(deltaTime){
+        super.update(deltaTime);
     }
     draw(context){
         context.drawImage(
@@ -60,7 +57,7 @@ class FlyingEnemy extends Enemy {
     }
 }
 
-class GroundEnemy extends Enemy {
+export class GroundEnemy extends Enemy {
     constructor(game){
         super();
         this.game = game;
@@ -96,7 +93,7 @@ class GroundEnemy extends Enemy {
     }
 }
 
-class ClimbingEnemy extends Enemy {
+export class ClimbingEnemy extends Enemy {
     constructor(game){
         super();
         this.game = game;
